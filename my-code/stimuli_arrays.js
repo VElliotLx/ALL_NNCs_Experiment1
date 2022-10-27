9328129e901d212925013379cfef2dc8d6473ec4
VElliotLx <103517872+VElliotLx@users.noreply.github.com>
2022-10-27 14:43:35 +0100

    Add files via upload

---
+++ my-code/stimuli_arrays.js
@@ -0,0 +1,37 @@
+/* meanings */
+var noun_meanings = [
+    "singular",
+    "dual",
+    "plural1",
+    "plural2"
+];
+
+/* apple learning */
+var images_apple = [
+    'images/Apple_learning_singular.png',
+    'images/Apple_learning_dual.png',
+    'images/Apple_learning_plural1.png',
+    'images/Apple_learning_plural2.png'
+];
+
+var words_apple = [
+    "pell",
+    "pellba",
+    "pellku",
+    "pellku"
+];
+
+/* banana learning */
+var images_banana = [
+    'images/Banana_learning_singular.png',
+    'images/Banana_learning_dual.png',
+    'images/Banana_learning_plural1.png',
+    'images/Banana_learning_plural2.png'
+];
+
+var words_banana = [
+    "blan",
+    "blanba",
+    "blanku",
+    "blanku"
+];
